19724ee8cd7015aa6b2e7ca8b639b26c64cf4dea
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-05-10 15:02:15 +0800

    still fixing client.js

--- client/client.js
+++ client/client.js
@@ -1,30 +1,36 @@
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
+const  grpc  = require("@grpc/grpc-js");
+const  protoLoader  = require("@grpc/proto-loader");
+// const { MailChatClient } = require("../grpc/grpc_grpc_pb");
 
 // Load the protobug difinition for gRPC service
-const packageDefinition = protoLoader.loadSync('../grpc/grpc.proto',{
-keepCase: true,
-longs: String,
-enums: String,
-defaults: true,
-oneofs: true
+const protoPath = __dirname + '/../grpc/grpc.proto';
+const packageDefinition = protoLoader.loadSync(protoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
 });
 
-// Create a gRPC client using the package definition
-const mailChatProto = grpc.loadPackageDefinition(packageDefinition).mailchat;
-const client = new mailChatProto.MailChat('localhost:50051', grpc.credentials.createInsecure());
+const protoDescriptor =  grpc.loadPackageDefinition(packageDefinition)
+const mailChat = protoDescriptor.mailchat;
+// Create a gRPC client (a stub)
+const client = new mailChat.MailChat(
+  "localhost:50051",
+  grpc.credentials.createInsecure()
+);
 
 // Call the SendEmail method on the gRPC service
 var mailRequest = {
-    sender: 'kenshi@example.com',
-    recipient: 'kenshikuo@example.com',
-    subject: 'Hello Kenshi',
-    body: 'World has changed a lot.'
+  sender: "kenshi@example.com",
+  recipient: "kenshikuo@example.com",
+  subject: "Hello Kenshi",
+  body: "World has changed a lot.",
 };
-client.SendEmail(mailRequest,(err,response)=>{
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(response)
+client.sendEmail(mailRequest, (err, response) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(response);
 });
